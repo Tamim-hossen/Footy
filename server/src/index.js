b1f4f294c22e5c5d70afdcd577eff4ff3c67a8d7
Tamim <tamimhossen003@gmail.com>
2025-04-29 12:43:18 +0600

    back

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const app= express()
 
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://footsy.up.railway.app'],
     credentials:true
     }))
 app.use(express.json({limit:"10mb"}))
